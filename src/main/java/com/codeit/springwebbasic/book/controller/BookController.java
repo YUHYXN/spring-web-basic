40d94314c36ed373f3dbd0a946f2d17ca0bf77c7
woou <hxx_ara@naver.com>
2025-10-21 18:05:41 +0900

    도서 단일 조회 로직 작성, 전역 예외 처리 클래스 소개

--- src/main/java/com/codeit/springwebbasic/book/controller/BookController.java
+++ src/main/java/com/codeit/springwebbasic/book/controller/BookController.java
@@ -3,11 +3,15 @@ package com.codeit.springwebbasic.book.controller;
 import com.codeit.springwebbasic.book.dto.request.BookCreatRequestDto;
 import com.codeit.springwebbasic.book.dto.response.BookResponseDto;
 import com.codeit.springwebbasic.book.entity.Book;
+import com.codeit.springwebbasic.book.repository.BookRepository;
 import com.codeit.springwebbasic.book.service.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 
 @RestController
@@ -30,6 +34,17 @@ public class BookController {
 
         Book book = bookService.createBook(requestDto);
         return BookResponseDto.from(book);
+
+    }
+
+    // 조회 요청
+    // Url : localhost:8080/books
+    // 메서드 이름 : getBooks
+    @GetMapping("/api/books/{id}")
+    public void getBooks(Long id){
+        Book book = BookRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 도서가 없습니다. id=" + id) );
+    }
 
     }
 
